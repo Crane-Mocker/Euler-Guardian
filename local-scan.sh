ef8351cc3f06ebe3fe7459b126e62042935b9274
Crane-Mocker <anaishuangc0conut@gmail.com>
2021-08-09 17:02:13 +0800

    local scan -> func

--- local-scan.sh
+++ local-scan.sh
@@ -6,255 +6,255 @@
 #########################################################################
 #!/bin/sh
 
-#PROGRAM_NAME="EG_local_scan"
+#PROGRAM_NAME="local_scan"
 #PROGRAM_AUTHOR="c0conut"
-
-#banner
-echo -e "\e[1;32m---------------------------------------\n"
-echo -e "Welcome to use Euler Guardian!\n"
-echo -e "---------------------------------------\n\033[0m"
 
 #######################################################################
 # check current id
 #######################################################################
+function CurrentIdChk() {
+	local CurrentId=""
+	if [ -x /usr/xpg4/bin/id ]; then #Solaris
+		CurrentId=$(/usr/xpg4/bin/id -u 2>/dev/null)
+	elif [ "$(uname)" = "SunOS" ]; then
+		CurrentId=$(id | tr '=' ' ' | tr '(' ' ' | awk '{ print $2 }' 2>/dev/null)
+	else #"$(uname)" = "Linux", for Euler, ubuntu, etc
+		CurrentId=$(id -u 2>/dev/null)
+	fi
 
-#check current id
-CurrentId=""
-if [ -x /usr/xpg4/bin/id ]; then #Solaris
-	CurrentId=$(/usr/xpg4/bin/id -u 2>/dev/null)
-elif [ "$(uname)" = "SunOS" ]; then
-	CurrentId=$(id | tr '=' ' ' | tr '(' ' ' | awk '{ print $2 }' 2>/dev/null)
-else #"$(uname)" = "Linux", for Euler, ubuntu, etc
-	CurrentId=$(id -u 2>/dev/null)
-fi
+	#check if UID = 0(root)
+	if [ ${CurrentId} -eq 0 ]; then
+		IsRoot=1
+		ScanMode=0
+	else
+		IsRoot=0
+		ScanMode=1
+	fi
+}
 
-#check if UID = 0(root)
-if [ ${CurrentId} -eq 0 ]; then
-	IsRoot=1
-	ScanMode=0
-else
-	IsRoot=0
-	ScanMode=1
-fi
 
 #####################################################################
-#path
+# path
+# check SetUID and get the path currently working on
 #####################################################################
-#check SetUID ("s")
-if [ -u "$0" ]; then
-	echo -e "\e[0;36mStopped because of unusual SetUID. Exit.\n\033[0m"
-	exit 1
-fi
-
-#pwd
-WorkDir=$(pwd)
+function GetPWD() {
+	#check SetUID ("s")
+	if [ -u "$0" ]; then
+		echo -e "\e[0;36mStopped because of unusual SetUID. Exit.\n\033[0m"
+		exit 1
+	fi
+	WorkDir=$(pwd)
+}
 
 ########################################################################
-# system info
+# system info check
+# kernel info, detailed kernel info, release info
 ########################################################################
-echo -e "\e[1;32mSystem information check starts...\n\033[0m"
+function SysInfoChk() {
+	# kernel info
+	local UnameInfo=`uname -a 2>/dev/null`	#`uname -a`
+	if [ "$UnameInfo" ]; then
+		# standard output and add to $report
+		echo -e "\e[1;34mKernel info:\e[00m\n$UnameInfo\n\033[0m" |tee -a $report 2>/dev/null
+	else
+		echo -e "\e[0;36mUname failed.\n\033[0m" |tee -a $report 2>/dev/null
+	fi
 
-# kernel info
-UnameInfo=`uname -a 2>/dev/null`	#`uname -a`
-if [ "$UnameInfo" ]; then
-	# standard output and add to $report
-	echo -e "\e[1;34mKernel info:\e[00m\n$UnameInfo\n\033[0m" |tee -a $report 2>/dev/null
-else
-	echo -e "\e[0;36mUname failed.\n\033[0m" |tee -a $report 2>/dev/null
-fi
+	# kernel version, gcc version to compile, time of compilation
+	local KernelVersion=`cat /proc/version 2>/dev/null`
+	if [ "$KernelVersion" ]; then
+		echo -e "\e[1;34mKernel version:\e[00m\n$KernelVersion\n\033[0m" |tee -a $report 2>/dev/null
+	else
+		echo -e "\e[0;36mcat /proc/version failed.\n\033[0m"|tee -a $report 2>/dev/null
+	fi
 
-# kernel version, gcc version to compile, time of compilation
-KernelVersion=`cat /proc/version 2>/dev/null`
-if [ "$KernelVersion" ]; then
-	echo -e "\e[1;34mKernel version:\e[00m\n$KernelVersion\n\033[0m" |tee -a $report 2>/dev/null
-else
-	echo -e "\e[0;36mcat /proc/version failed.\n\033[0m"|tee -a $report 2>/dev/null
-fi
-
-#release info
-ReleaseInfo=`cat /etc/*-release 2>/dev/null`
-if [ "$ReleaseInfo" ]; then
-	echo -e "\e[1;34mrelease info:\e[00m\n$ReleaseInfo\n\033[0m" |tee -a $report 2>/dev/null
-else
-	echo -e "\e[0;36mcat /etc/*-release failed.\n\033[0m"|tee -a $report 2>/dev/null
-fi
-
+	#release info
+	local ReleaseInfo=`cat /etc/*-release 2>/dev/null`
+	if [ "$ReleaseInfo" ]; then
+		echo -e "\e[1;34mrelease info:\e[00m\n$ReleaseInfo\n\033[0m" |tee -a $report 2>/dev/null
+	else
+		echo -e "\e[0;36mcat /etc/*-release failed.\n\033[0m"|tee -a $report 2>/dev/null
+	fi
+}
 
 ########################################################################
 # user info
 ########################################################################
-echo -e "\e[1;32mUser information check starts...\n\033[0m"
-
-# hostname
-Hostname=`hostname 2>/dev/null`
-if [ "$Hostname" ]; then
-	echo -e "\e[1;34mHostname:\e[00m\n$Hostname\n\033[0m" |tee -a $report 2>/dev/null
-else
-	echo -e "\e[0;36mhostname failed.\n\033[0m"|tee -a $report 2>/dev/null
-fi
-
-#id
-Id=`id 2>/dev/null`
-if [ "$Id" ]; then
-	echo -e "\e[1;34mCurrent user and group IDs:\e[00m\n$Id\n\033[0m" |tee -a $report 2>/dev/null
-else
-	echo -e "\e[0;36mid failed.\n\033[0m"|tee -a $report 2>/dev/null
-fi
-
-#user accounts info
-Passwd=`cat /etc/passwd | cut -d ":" -f 1,2,3,4 2>/dev/null`
-if [ "$Passwd" ]; then
-	echo -e "\e[1;34mUsers and permissions:\033[0m"
-	echo -e "\e[0;34mUsername:Password:UID:GID\033[0m"
-	echo -e "\e[00m$Passwd\n\033[0m" |tee -a $report 2>/dev/null
-	#group memebership
-	GroupIdInfo=`for i in $(cat /etc/passwd 2>/dev/null| cut -d":" -f1 2>/dev/null);do id $i;done 2>/dev/null`
-	if [ "$GroupIdInfo" ]; then
-		echo -e "\e[1;34mGroup memberships:\e[00m\n$GroupIdInfo\n\033[0m" |tee -a $report 2>/dev/null
+function UserInfoChk() {
+	# hostname
+	local Hostname=`hostname 2>/dev/null`
+	if [ "$Hostname" ]; then
+		echo -e "\e[1;34mHostname:\e[00m\n$Hostname\n\033[0m" |tee -a $report 2>/dev/null
 	else
-		:
+		echo -e "\e[0;36mhostname failed.\n\033[0m"|tee -a $report 2>/dev/null
 	fi
-	#if password stored in /etc/passwd as hash
-	HashPw=`grep -v '^[^:]*:[x]' /etc/passwd 2>/dev/null`
-	if [ "$HashPw" ]; then
-		echo -e "\e[1;34mFound password stored in /etc/passwd as hash:\n\033[0m$HashPw\n" | tee -a $report 2>/dev/null
-	else
-		echo -e "\e[0;34mNo password is stored in /etc/passwd as hash.\n\033[0m" |tee -a $report 2>/dev/null
-	fi
-else
-	echo -e "\e[0;36m cat /etc/passwd failed.\n\033[0m" |tee -a $report 2>/dev/null
-fi
 
-#last log for each user
-LastLogUser=`lastlog | grep -v "Never" 2>/dev/null`
-if [ "$LastLogUser" ]; then
-	echo -e "\e[1;34mUsers previously logged onto system:\e[0m\n$LastLogUser\n\033[0m" |tee -a $report 2>/dev/null
-else
-	echo -e "\e[0;36mCan't find /var/log/lastlog.\n\033[0m"|tee -a $report 2>/dev/null
-fi
+	#id
+	local Id=`id 2>/dev/null`
+	if [ "$Id" ]; then
+		echo -e "\e[1;34mCurrent user and group IDs:\e[00m\n$Id\n\033[0m" |tee -a $report 2>/dev/null
+	else
+		echo -e "\e[0;36mid failed.\n\033[0m"|tee -a $report 2>/dev/null
+	fi
+
+	#user accounts info
+	local Passwd=`cat /etc/passwd | cut -d ":" -f 1,2,3,4 2>/dev/null`
+	if [ "$Passwd" ]; then
+		echo -e "\e[1;34mUsers and permissions:\033[0m"
+		echo -e "\e[0;34mUsername:Password:UID:GID\033[0m"
+		echo -e "\e[00m$Passwd\n\033[0m" |tee -a $report 2>/dev/null
+		#group memebership
+		local GroupIdInfo=`for i in $(cat /etc/passwd 2>/dev/null| cut -d":" -f1 2>/dev/null);do id $i;done 2>/dev/null`
+		if [ "$GroupIdInfo" ]; then
+			echo -e "\e[1;34mGroup memberships:\e[00m\n$GroupIdInfo\n\033[0m" |tee -a $report 2>/dev/null
+		else
+			:
+		fi
+		#if password stored in /etc/passwd as hash
+		local HashPw=`grep -v '^[^:]*:[x]' /etc/passwd 2>/dev/null`
+		if [ "$HashPw" ]; then
+			echo -e "\e[1;34mFound password stored in /etc/passwd as hash:\n\033[0m$HashPw\n" | tee -a $report 2>/dev/null
+		else
+			echo -e "\e[0;34mNo password is stored in /etc/passwd as hash.\n\033[0m" |tee -a $report 2>/dev/null
+		fi
+	else
+		echo -e "\e[0;36m cat /etc/passwd failed.\n\033[0m" |tee -a $report 2>/dev/null
+	fi
+
+	#last log for each user
+	local LastLogUser=`lastlog | grep -v "Never" 2>/dev/null`
+	if [ "$LastLogUser" ]; then
+		echo -e "\e[1;34mUsers previously logged onto system:\e[0m\n$LastLogUser\n\033[0m" |tee -a $report 2>/dev/null
+	else
+		echo -e "\e[0;36mCan't find /var/log/lastlog.\n\033[0m"|tee -a $report 2>/dev/null
+	fi
+}
+
 
 #######################################################################
 #file permission/ownership check
 #######################################################################
-echo -e "\e[1;32mFiles permission and ownership check starts...\033[0m"
+function FilePermChk() {
+	echo -e "\e[1;32mFiles permission and ownership check starts...\033[0m"
 
-FilesPath="consts functions"
-Issue=0
-IssueType=0
-ShowPermissionErr=0 # 1-currently scan is not run by root
-IncludeDir="" #with bash files to do tests
+	FilesPath="consts functions"
+	Issue=0
+	IssueType=0
+	ShowPermissionErr=0 # 1-currently scan is not run by root
+	IncludeDir="" #with bash files to do tests
 
-
-
-for File in ${FilesPath}; do
-	FilePermission=$(ls -l ${IncludeDir}/${File} | cut -c 2-10)
-	GroupPermission=$(ls -l ${IncludeDir}/${File} | cut -c 5-7)
-	GroupOwnerId=$(ls -n ${IncludeDir}/${File} | awk '{print $4}')
-	Owner=$(ls -l ${IncludeDir}/${File} | awk -F" " '{print $3}')
-	OwnerId=$(ls -n ${IncludeDir}/${File} | awk -F" " '{print $3}')
-	#check permissions of include files
-	#for files has alll rwx
-	if [ "${FilePermission}" = "rwxrwxrwx" ]; then
-		Issue=1
-		IssueType="perms"
-		echo -e "\e[1;34mChange file permissions of ${IncludeDir}/${File} to 640.\033[0m"
-		#echo "Command: chmod 640 ${IncludeDir}/${File}"
-	#if group owner id=owner id, consider it as defualt umask
-	elif [ ! "${FilePermission}"="r--------" -a ! "${FilePermission}"="rw-------"-a!"${FilePermission}"="rw-r-----" -a ! "${FilePermission}"="rw-r--r--" ];then
-		if [ ! "${GroupOwnerId}" = "${OwnerId}" ]; then
-			Issue=1;
-			IssueType="perms"
-			echo -e "\033[0mChange file permissions of ${IncludeDir}/${File} to 640."
-		fi
-	fi
-
-	#check if it's root user to run scan
-	if [ ! "${Owner}" = "root" -a ! "${OwnerId}" = "0" ]; then
-		if [ ! "${CurrentId}" = "${OwnerId}"]; then
+	for File in ${FilesPath}; do
+		FilePermission=$(ls -l ${IncludeDir}/${File} | cut -c 2-10)
+		GroupPermission=$(ls -l ${IncludeDir}/${File} | cut -c 5-7)
+		GroupOwnerId=$(ls -n ${IncludeDir}/${File} | awk '{print $4}')
+		Owner=$(ls -l ${IncludeDir}/${File} | awk -F" " '{print $3}')
+		OwnerId=$(ls -n ${IncludeDir}/${File} | awk -F" " '{print $3}')
+		#check permissions of include files
+		#for files has alll rwx
+		if [ "${FilePermission}" = "rwxrwxrwx" ]; then
 			Issue=1
-			IssueType="owner"
-			ShowPermissionErr=1;
-			IssueFile="${File}" #the file with issue
-			IssueOwner="${Owner}"
-			IssueOwnerId="${OwnerId}"
+			IssueType="perms"
+			echo -e "\e[1;34mChange file permissions of ${IncludeDir}/${File} to 640.\033[0m"
+			#echo "Command: chmod 640 ${IncludeDir}/${File}"
+		#if group owner id=owner id, consider it as defualt umask
+		elif [ ! "${FilePermission}"="r--------" -a ! "${FilePermission}"="rw-------"-a!"${FilePermission}"="rw-r-----" -a ! "${FilePermission}"="rw-r--r--" ];then
+			if [ ! "${GroupOwnerId}" = "${OwnerId}" ]; then
+				Issue=1;
+				IssueType="perms"
+				echo -e "\033[0mChange file permissions of ${IncludeDir}/${File} to 640."
+			fi
 		fi
+
+		#check if it's root user to run scan
+		if [ ! "${Owner}" = "root" -a ! "${OwnerId}" = "0" ]; then
+			if [ ! "${CurrentId}" = "${OwnerId}" ]; then
+				Issue=1
+				IssueType="owner"
+				ShowPermissionErr=1;
+				IssueFile="${File}" #the file with issue
+				IssueOwner="${Owner}"
+				IssueOwnerId="${OwnerId}"
+			fi
+		fi
+	done
+
+	#if scan isn't run by Root
+	if [ ${ShowPermissionErr} -eq 1 ]; then
+		print "%s" "
+
+	[!] Change ownership of ${IncludeDir}/${IssueFile} to 'root' or similar (found: ${IssueOwner} with UID ${IssueOwnerId}).
+
+	Command:
+	  # chown 0:0 ${IncludeDir}/${IssueFile}
+	"
 	fi
-done
-
-#if scan isn't run by Root
-if [ ${ShowPermissionErr} -eq 1 ]; then
-	print "%s" "
-
-[!] Change ownership of ${IncludeDir}/${IssueFile} to 'root' or similar (found: ${IssueOwner} with UID ${IssueOwnerId}).
-
-Command:
-  # chown 0:0 ${IncludeDir}/${IssueFile}
-"
-fi
+}
 
 ####################################################################
 # 软件包版本漏洞检查
 ####################################################################
+function OVALChk() {
+	# 检查使用的包管理器, 安装oscap
+	if [ "$(apt -v 2>/dev/null)" ]; then
+		#使用apt作为包管理器
+		echo -e "\e[1;34mThis device uses apt.\n\033[0m" |tee -a $report 2>/dev/null
+		# 检查是否有oscap工具
+		if [ "$(oscap -h 2>/dev/null)" ]; then
+			:
+		else
+			echo -e "\e[1;34mNo oscap. Downloading...\n\033[0m" |tee -a $report 2>/dev/null
+			sudo apt-get install libopenscap8
+		fi
+	elif [ "$(yum --version 2>/dev/null)" ]; then
+		# 使用yum作为包管理器
+		echo -e "\e[1;34mThis device uses yum.\n\033[0m" |tee -a $report 2>/dev/null
 
-# 检查使用的包管理器, 安装oscap
-if [ "$(apt -v 2>/dev/null)" ]; then
-	#使用apt作为包管理器
-	echo -e "\e[1;34mThis device uses apt.\n\033[0m" |tee -a $report 2>/dev/null
-	# 检查是否有oscap工具
-	if [ "$(oscap -h 2>/dev/null)" ]; then
-		:
-	else
-		echo -e "\e[1;34mNo oscap. Downloading...\n\033[0m" |tee -a $report 2>/dev/null
-		sudo apt-get install libopenscap8
+		# 检查是否有oscap工具
+		if [ "$(oscap -h 2>/dev/null)" ]; then
+			:
+		else
+			echo -e "\e[1;34mNo oscap. Downloading...\n\033[0m" |tee -a $report 2>/dev/null
+			sudo yum install openscap-utils
+		fi
 	fi
-elif [ "$(yum --version 2>/dev/null)" ]; then
-	# 使用yum作为包管理器
-	echo -e "\e[1;34mThis device uses yum.\n\033[0m" |tee -a $report 2>/dev/null
 
-	# 检查是否有oscap工具
-	if [ "$(oscap -h 2>/dev/null)" ]; then
-		:
-	else
-		echo -e "\e[1;34mNo oscap. Downloading...\n\033[0m" |tee -a $report 2>/dev/null
-		sudo yum install openscap-utils
-	fi
-fi
+	# 检查oval文件
+	# release id
+	tmpStr=`cat /etc/*-release | grep ^ID=`
+	IFS='='
+	read -ra tmpArr <<<"$tmpStr"
+	releaseIDStr=${tmpArr[1]}
+	#echo "$releaseIDStr"
 
-# 检查oval文件
-# release id
-tmpStr=`cat /etc/*-release | grep ^ID=`
-IFS='='
-read -ra tmpArr <<<"$tmpStr"
-releaseIDStr=${tmpArr[1]}
-#echo "$releaseIDStr"
+	# release version id
+	tmpStr=`cat /etc/*-release | grep VERSION_ID 2>/dev/null`
+	IFS='"'
+	read -ra tmpArr <<<"$tmpStr"
+	releaseVersionID=${tmpArr[1]}
+	releaseVersionIDStr=`echo ${releaseVersionID//./}`
+	#echo "$releaseVersionIDStr"
 
-# release version id
-tmpStr=`cat /etc/*-release | grep VERSION_ID 2>/dev/null`
-IFS='"'
-read -ra tmpArr <<<"$tmpStr"
-releaseVersionID=${tmpArr[1]}
-releaseVersionIDStr=`echo ${releaseVersionID//./}`
-#echo "$releaseVersionIDStr"
-
-targetOVALFile="${releaseIDStr}_${releaseVersionIDStr}.xml"
-#echo "$targetOVALFile"
-hasOVALFile=`ls | grep ${targetOVALFile} 2>/dev/null`
-if [ "$hasOVALFile" ]; then
-	echo -e "\e[1;34mOVAL file found:\e[00m\n$targetOVALFile\n\033[0m" |tee -a $report 2>/dev/null
-else
-	echo -e "\e[0;36mNo target OVALFile found. Downloading...\n\033[0m" |tee -a $report 2>/dev/null
-	wget https://oval.cisecurity.org/repository/download/5.11.2/vulnerability/${targetOVALFile}
+	targetOVALFile="${releaseIDStr}_${releaseVersionIDStr}.xml"
+	#echo "$targetOVALFile"
 	hasOVALFile=`ls | grep ${targetOVALFile} 2>/dev/null`
-	if [ "hasOVALFile" ]; then
-		:
+	if [ "$hasOVALFile" ]; then
+		echo -e "\e[1;34mOVAL file found:\e[00m\n$targetOVALFile\n\033[0m" |tee -a $report 2>/dev/null
 	else
-		wget https://oval.cisecurity.org/repository/download/5.11.2/vulnerability/centos_linux_73.xml
-		targetOVALFile="centos_linux_73.xml"
+		echo -e "\e[0;36mNo target OVALFile found. Downloading...\n\033[0m" |tee -a $report 2>/dev/null
+		wget https://oval.cisecurity.org/repository/download/5.11.2/vulnerability/${targetOVALFile}
+		hasOVALFile=`ls | grep ${targetOVALFile} 2>/dev/null`
+		if [ "hasOVALFile" ]; then
+			:
+		else
+			wget https://oval.cisecurity.org/repository/download/5.11.2/vulnerability/centos_linux_73.xml
+			targetOVALFile="centos_linux_73.xml"
+		fi
 	fi
-fi
 
-oscap oval eval --results ./oscap_results.xml --report ./oscap_report.html ${targetOVALFile}
-echo -e "\e[1;34mPlease check for vuln scan results in oscap_results.xml and oscap_report.html\n\033[0m" |tee -a $report 2>/dev/null
+	oscap oval eval --results ./oscap_results.xml --report ./oscap_report.html ${targetOVALFile}
+	echo -e "\e[1;34mPlease check for vuln scan results in oscap_results.xml and oscap_report.html\n\033[0m" |tee -a $report 2>/dev/null
+}
+
 
 ########################################################################
 # log auditing
@@ -303,7 +303,7 @@ function Function {
 # MountOption [filesystem挂载点对应的文件系统] [mountOption挂载选项]
 #####################################################################
 
-function MountOption {
+function MountOption() {
 	local filesystem="${1}"
 	local mountOption="${2}"
 	#执行命令，失败则return
@@ -314,7 +314,7 @@ function MountOption {
 ####################################################################
 # selinux检查
 ####################################################################
-function SELinuxCheck {
+function SELinuxCheck() {
 	#if SElinux is disabled in grub.cfg selinux=0关闭
 	local SelinuxEq0="$(grep selinux=0 ${GRUB_CFG})"
 	[[ -z "${SelinuxEq0}" ]] || return
@@ -326,14 +326,38 @@ function SELinuxCheck {
 # 自定义函数
 ###################################################################
 
-#function myFunction {
+#function myFunction() {
 #	local myLocalVar=
 #	:
 #}
 
-####################################################################
-# 主函数
-####################################################################
-function main {
-	:
-}
+#####################################################################
+#  程序开始
+#####################################################################
+
+#banner
+echo -e "\e[1;32m---------------------------------------\n"
+echo -e "Welcome to use Euler Guardian!\n"
+echo -e "---------------------------------------\n\033[0m"
+
+echo -e "\e[1;34m\n-----------------------------------------------"
+echo "System information check start"
+echo -e "-----------------------------------------------\033[0m\n"
+CurrentIdChk
+GetPWD
+SysInfoChk
+
+echo -e "\e[1;34m\n-----------------------------------------------"
+echo "Users information check start"
+echo -e "-----------------------------------------------\033[0m\n"
+UserInfoChk
+
+echo -e "\e[1;34m\n-----------------------------------------------"
+echo "Files permissions check start"
+echo -e "-----------------------------------------------\033[0m\n"
+FilePermChk
+
+echo -e "\e[1;34m\n-----------------------------------------------"
+echo "Software vuln check start"
+echo -e "-----------------------------------------------\033[0m\n"
+OVALChk
