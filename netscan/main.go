efe77bd9ffaf273812d2972ced2c450a76a7e252
Crane-Mocker <anaishuangc0conut@gmail.com>
2021-09-30 01:36:10 +0800

    net scan info display

---
+++ netscan/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	//"time"
+	"flag"
+)
+
+var (
+	cliHelp = flag.Bool("h", false, "Help")
+)
+
+func main(){
+	fmt.Println("\033[1;32m-----------------------------------------------")
+	fmt.Println(" ___         __              ")
+	fmt.Println("(_    /_ _  / _   _ _ _/'_   ")
+	fmt.Println("/__(/((-/  (__)(/(// (//(//) ")
+	fmt.Println("Welcome to use Euler Guardian!")
+	fmt.Println("This is the net scan module.")
+	fmt.Println("-----------------------------------------------\033[0m")
+
+	//timeStamp := time.Now().Unix()
+	flag.Parse()
+	if *cliHelp == true {
+		fmt.Println("This is the net scan module of Euler Guardian.")
+		fmt.Println("Usage:")
+	}
+}
